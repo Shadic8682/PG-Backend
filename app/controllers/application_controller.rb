4d528bba853b32dc05e226f49c3aac49136f4a14
Khanya Keswa <khanya.keswa@gmail.com>
2022-12-07 13:14:49 -0600

    controller push req edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,26 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
-  get "/test" do
-    { message: "Good luck with your project!" }.to_json
+  get '/test' do
+    { message: 'Good luck with your project!' }.to_json
   end
 
   # pulls up all the gardener instances
-  get "/gardeners" do
+  get '/gardeners' do
     Gardener.all.to_json(include: :gardens)
   end
 
-  get "/gardens" do
+  get '/gardens' do
     Garden.all.to_json(include: :crops)
   end
 
-  get "/crops" do
+  get '/crops' do
     Crop.all.to_json
   end
 
-  post "gardeners" do
-    newGardener = Gardener.create(params)
-    newGardener.to_json
+  post '/gardeners' do
+    new_gardener = Gardener.create(name: params[:name])
+    new_gardener.to_json
   end
-
 end
